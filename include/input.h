f519fd0142d663e27cf1ec09db8fae18afdfea4e
Wingnut0021 <wingnut0021@live.com>
2024-01-18 15:39:53 +1000

    refactor: Input X and Y direction seperation

--- include/input.h
+++ include/input.h
@@ -3,18 +3,9 @@
 
 #include <gb/gb.h>
 
-void getInput_BUTTON_A(void);
-void getInput_BUTTON_B(void);
-void getInput_BUTTON_START(void);
-void getInput_BUTTON_SELECT(void);
-void getInput_BUTTON_UP(void);
-void getInput_BUTTON_DOWN(void);
-void getInput_BUTTON_LEFT(void);
-void getInput_BUTTON_RIGHT(void);
-void getInput_BUTTON_UP_RIGHT(void);
-void getInput_BUTTON_UP_LEFT(void);
-void getInput_BUTTON_DOWN_RIGHT(void);
-void getInput_BUTTON_DOWN_LEFT(void);
-void getInput(void);
+
+
+int getXDirectionInput(int x);
+int getYDirectionInput(int x);
 
 #endif
--- src/core/input.c
+++ src/core/input.c
@@ -1,92 +1,28 @@
 #include <gb/gb.h>
+#include "input.h"
 
 
 
-void getInput_BUTTON_A(void) {
-
-}
-
-void getInput_BUTTON_B(void) {
-
-}
-
-void getInput_BUTTON_START(void) {
-
-}
-
-void getInput_BUTTON_SELECT(void) {
-
-}
-
-void getInput_BUTTON_UP(void) {
-
-}
-
-void getInput_BUTTON_DOWN(void) {
-
-}
-
-void getInput_BUTTON_LEFT(void) {
-
-}
-
-void getInput_BUTTON_RIGHT(void) {
-
-}
-
-void getInput_BUTTON_UP_RIGHT(void) {
-
-}
-
-void getInput_BUTTON_UP_LEFT(void) {
-
-}
-
-void getInput_BUTTON_DOWN_RIGHT(void) {
-
-}
-
-void getInput_BUTTON_DOWN_LEFT(void) {
-
-}
-
-void getInput(void) {
+int getXDirectionInput(int x) {
     UBYTE input = joypad();
-
-    if (input & J_A) {
-        getInput_BUTTON_A();
-    }
-    if (input & J_B) {
-        getInput_BUTTON_B();
-    }
-    if (input & J_START) {
-        getInput_BUTTON_START();
-    }
-    if (input & J_SELECT) {
-        getInput_BUTTON_SELECT();
-    }
-    if (input & J_UP) {
-        getInput_BUTTON_UP();
-    }
-    if (input & J_DOWN) {
-        getInput_BUTTON_DOWN();
-    }
-    if (input & J_RIGHT) {
-        getInput_BUTTON_RIGHT();
-    }
     if (input & J_LEFT) {
-        getInput_BUTTON_LEFT();
+        x = -1;
+        return x;
+    } else if (input & J_RIGHT) {
+        x = 1;
+        return x;
     }
-    if ((input & J_UP) && (input & J_RIGHT)) {
-        getInput_BUTTON_UP_RIGHT();
+    return x;
+}
+
+int getYDirectionInput(int y) {
+    UBYTE input = joypad();
+    if (input & J_UP) {
+        y = -1;
+        return y;
+    } else if (input & J_DOWN) {
+        y = 1;
+        return y;
     }
-    if ((input & J_UP) && (input & J_LEFT)) {
-        getInput_BUTTON_UP_LEFT();
-    }
-    if ((input & J_DOWN) && (input & J_RIGHT)) {
-        getInput_BUTTON_DOWN_RIGHT();
-    }
-    if ((input & J_DOWN) && (input & J_LEFT)) {
-        getInput_BUTTON_DOWN_LEFT();
-    }
+    return y;
 }
