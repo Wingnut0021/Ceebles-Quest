254e5eea2ffe8978e7080ba4ddf6dacbafeb5425
Wingnut0021 <wingnut0021@live.com>
2024-01-20 16:34:54 +1000

    fix: fix collision variable name.

--- include/bg_samplemap_collision.h
+++ include/bg_samplemap_collision.h
@@ -1,4 +1,4 @@
 #define bg_samplemap_collision_width 20
 #define bg_samplemap_collision_height 18
 
-extern const unsigned char bg_collision_tilemap[];
+extern const unsigned char bg_samplemap_collision[];
