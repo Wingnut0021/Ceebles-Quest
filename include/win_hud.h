4736c84a35c8e31189c57adf3ee59b8b85abaa5e
Wingnut0021 <wingnut0021@live.com>
2024-01-29 21:59:27 +1000

    feat: Hud movement

--- include/win_hud.h
+++ include/win_hud.h
@@ -4,8 +4,6 @@
 #include <gb/gb.h>
 
 void initializeHud(void);
-void moveHudUP(void);
-void moveHudDOWN(void);
 void updateHud(void);
 
 #endif
--- src/core/win_hud.c
+++ src/core/win_hud.c
@@ -1,27 +1,41 @@
 #include <gb/gb.h>
+#include <gbdk/emu_debug.h>
 #include <stdio.h>
 
+#include "input.h"
 #include "bg_hud_tileset.h"
 #include "bg_hud_tilemap.h"
 
 #define hudDownPosition 128
 #define hudUpPosition 0
 
+uint8_t hudCurrentPosition;
 
-void moveHudUP(void) {
-    move_win(0, hudUpPosition);
-}
-void moveHudDOWN(void) {
-    move_win(0, hudDownPosition);
-}
 
 void initializeHud(void) {
-    set_win_tiles(0, 0, 5, 1, bg_hud_tilemap);
-    moveHudDOWN();
+    set_win_tiles(0, 0, 20, 18, bg_hud_tilemap);
+    move_win(0, hudDownPosition);
+    scroll_win(7,0); // This seems hacky,
+    hudCurrentPosition = hudDownPosition;
     SHOW_WIN;
 }
 
-
 void updateHud(void) {
-
+    uint8_t inputStart = getSTARTInput(inputStart);
+    if (inputStart == 255 && !hudCurrentPosition == 0) {
+        scroll_win(0, -2);
+        hudCurrentPosition -= 2;
+        HIDE_SPRITES;
+    }
+    else if (inputStart == 255 && hudCurrentPosition == 0) {
+        scroll_win(0, 0);
+        hudCurrentPosition -= 0;
+    }
+    else if (hudCurrentPosition == 128) {
+        SHOW_SPRITES;
+    }
+    else {
+        scroll_win(0, 2);
+        hudCurrentPosition += 2;
+    }
 }
