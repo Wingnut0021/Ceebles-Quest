11d0d175ffe6dd504b8d0ce28a3624731ad57fa9
Wingnut0021 <wingnut0021@live.com>
2024-01-29 18:32:53 +1000

    feat: metasprite struct

---
+++ include/spritemetasprite.h
@@ -0,0 +1,12 @@
+#ifndef SPRITEMETASPRITE_H
+#define SPRITEMETASPRITE_H
+
+struct SpriteMetaSprite {
+    UBYTE spriteids[4];
+    uint8_t x;
+    uint8_t y;
+    uint8_t width;
+    uint8_t height;
+};
+
+#endif
---
+++ src/core/spritemetasprite.c
@@ -0,0 +1,9 @@
+#include <gb/gb.h>
+
+struct SpriteMetaSprite {
+    UBYTE spriteids[4];
+    uint8_t x;
+    uint8_t y;
+    uint8_t width;
+    uint8_t height;
+};
