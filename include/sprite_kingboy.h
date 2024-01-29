49cd01b1bd6bc0f90febf2502dcfe40a8e92ba7b
Wingnut0021 <wingnut0021@live.com>
2024-01-29 18:31:10 +1000

    feat: kingboy sprite

---
+++ include/sprite_kingboy.h
@@ -0,0 +1,34 @@
+#ifndef SPRITE_KINGBOY_H
+#define SPRITE_KINGBOY_H
+
+#define TILE_COUNT 48
+#define TILE_SLICE_01 0x0000
+#define TILE_SLICE_02 0x0002
+#define TILE_SLICE_03 0x0004
+#define TILE_SLICE_04 0x0006
+#define TILE_SLICE_05 0x0008
+#define TILE_SLICE_06 0x000a
+#define TILE_SLICE_07 0x000c
+#define TILE_SLICE_08 0x000e
+#define TILE_SLICE_09 0x0010
+#define TILE_SLICE_10 0x0012
+#define TILE_SLICE_11 0x0014
+#define TILE_SLICE_12 0x0016
+#define TILE_SLICE_13 0x0018
+#define TILE_SLICE_14 0x001a
+#define TILE_SLICE_15 0x001c
+#define TILE_SLICE_16 0x001e
+#define TILE_SLICE_17 0x0020
+#define TILE_SLICE_18 0x0022
+#define TILE_SLICE_19 0x0024
+#define TILE_SLICE_20 0x0026
+#define TILE_SLICE_21 0x0028
+#define TILE_SLICE_22 0x002a
+#define TILE_SLICE_23 0x002c
+#define TILE_SLICE_24 0x002e
+
+
+
+extern const unsigned char sprite_kingboy[];
+
+#endif
