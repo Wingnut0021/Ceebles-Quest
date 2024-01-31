a6cc6b2d22c65b1fa5c226420d839b5b3d967680
Wingnut0021 <wingnut0021@live.com>
2024-01-31 11:51:56 +1000

    feat: sound effects for opening and closing menu

--- include/sound_effects.h
+++ include/sound_effects.h
@@ -1,7 +1,11 @@
 #ifndef SOUND_EFFECTS_H
 #define SOUND_EFFECTS_H
 
+
 void soundCore(void);
 void startSound(void);
+void openMenuSound(void);
+void closeMenuSound(void);
+void attackSound(void);
 
 #endif
--- src/core/sound_effects.c
+++ src/core/sound_effects.c
@@ -1,5 +1,7 @@
 #include <gb/gb.h>
 
+uint8_t duration = 0;
+
 void soundCore(void) {
     NR52_REG = 0x80; // Turns on sound
     NR50_REG = 0x77; // Sets volume to max
@@ -15,4 +17,29 @@ void startSound(void) {
     NR14_REG = 0xC3;
 
     delay(1000);
+}
+
+void openMenuSound(void) {
+    NR10_REG = 0x16;
+    NR11_REG = 0x40;
+    NR12_REG = 0x73;
+    NR13_REG = 0x00;
+    NR14_REG = 0xC3;
+}
+
+void closeMenuSound(void) {
+    NR10_REG = 0x06;
+    NR11_REG = 0x40;
+    NR12_REG = 0x73;
+    NR13_REG = 0x00;
+    NR14_REG = 0xC3;
+}
+
+void attackSound(void) {
+    NR10_REG = 0x16;
+    NR11_REG = 0x10;
+    NR12_REG = 0x73;
+    NR13_REG = 0x00;
+    NR14_REG = 0xC3;
+    duration = 3;
 }
